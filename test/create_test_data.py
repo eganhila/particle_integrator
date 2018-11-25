d896769a1e6e1e877698385e1730bc331719a99e
Hilary Egan <hilaryye@gmail.com>
2018-11-25 13:06:05 -0700

    added script to create needed test data

---
+++ test/create_test_data.py
@@ -0,0 +1,28 @@
+import h5py
+import numpy as np
+
+
+data_i = np.zeros((3,3,3))
+data_j = np.zeros((3,3,3))
+data_k = np.zeros((3,3,3))
+data_0 = np.zeros((3,3,3))
+x = np.array(range(3), dtype=float)
+
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            data_i[i,j,k] = i
+            data_j[i,j,k] = j
+            data_k[i,j,k] = k
+
+
+with h5py.File("read_test.h5", "w") as f:
+    f.create_dataset("x", data=x)
+    f.create_dataset("y", data=x/2)
+    f.create_dataset("z", data=x+1)
+    f.create_dataset("magnetic_field_x", data=data_i)
+    f.create_dataset("magnetic_field_y", data=data_j)
+    f.create_dataset("magnetic_field_z", data=data_k)
+    f.create_dataset("electron_velocity_x", data=data_0+2)
+    f.create_dataset("electron_velocity_y", data=data_0+1)
+    f.create_dataset("electron_velocity_z", data=data_0)
